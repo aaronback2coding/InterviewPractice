b33ecee5fc693cf31cea72bbe7dc26df9e972f6c
Aaron (Li Bo) Tao <aarontao@fb.com>
2019-11-08 18:40:33 -0800

    Common ancestor initial

---
+++ CrackingCodingInterview/TreesAndGraphs/CommonAncestor/src/com/aaron/Main.java
@@ -0,0 +1,11 @@
+package com.aaron;
+//----------------------------------------------------------------------------------------------------------------------------
+//Find the first common ancestor of two nodes, avoid storing additional nodes in a data structure
+
+//----------------------------------------------------------------------------------------------------------------------------
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
