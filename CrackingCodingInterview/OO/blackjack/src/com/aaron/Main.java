1cf8d2269e75c0397457927da9db13d8da1d5d70
Aaron (Li Bo) Tao <aarontao@fb.com>
2019-11-19 15:27:05 -0800

    leanring on oo design

--- CrackingCodingInterview/OO/blackjack/src/com/aaron/Main.java
+++ CrackingCodingInterview/OO/blackjack/src/com/aaron/Main.java
@@ -1,4 +1,14 @@
 package com.aaron;
+// Learning
+//  the core concept of OO design:
+//      model the design as objects and their associations - game, hand, deck, card
+//      pursue abstraction, inheritance, and polymorphism for data reuse - game specific logic vs. game generics
+//      decouple and encapsulate data as needed
+//      the actual implementation using Java
+//      leanring - when you write code, try to write generic code if possible. it will make people feel good about your design
+//      the basics of business logic vs. code logic separation
+
+
 
 import com.aaron.blackjack.BlackJackGame;
 import com.aaron.blackjack.BlackJackHandFactory;
