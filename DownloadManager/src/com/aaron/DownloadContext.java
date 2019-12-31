25aeaf4bbd3849e7d7980d367772a45ddbebfbce
Li Bo Tao <Li.Tao@Oculus.com>
2019-12-30 19:11:54 -0800

    Download manager part 2

--- DownloadManager/src/com/aaron/DownloadContext.java
+++ DownloadManager/src/com/aaron/DownloadContext.java
@@ -2,33 +2,117 @@ package com.aaron;
 
 //To do - split download context into internal state management vs. external download progress tracking
 
-public class DownloadContext {
-    String URL;
-    String localFile;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class DownloadContext implements DownloadProgress{
+    String URL = null;
+    String localFile = null;
+    DownloadProgressCallback callback = null;
+
+    private volatile int downloadPercentage = 0; // 0 - 100.
+    private volatile DownloadStatus status = DownloadStatus.WAIT;
+    private ReentrantLock statusUpdateLock = null;
+    private Condition pausedCondition = null;
+
 
     public DownloadContext(String URL, String localFile) {
         this.URL = URL;
         this.localFile = localFile;
+        downloadPercentage = 0;
+        statusUpdateLock = new ReentrantLock();
+        pausedCondition = statusUpdateLock.newCondition();
     }
 
+    public DownloadContext(String URL, String localFile, DownloadProgressCallback callback) {
+        this.URL = URL;
+        this.localFile = localFile;
+        downloadPercentage = 0;
+        statusUpdateLock = new ReentrantLock();
+        pausedCondition = statusUpdateLock.newCondition();
+        this.callback = callback;
+    }
+
+    @Override
     public void pause() {
+        statusUpdateLock.lock();
+        try {
+            status = DownloadStatus.PAUSED;
+            pausedCondition.signalAll();
+        } finally {
+            statusUpdateLock.unlock();
+        }
+    }
+
+    void waitIfPaused() {
+        statusUpdateLock.lock();
+        try {
+            while (status == DownloadStatus.PAUSED) {
+                try {
+                    pausedCondition.await();
+                } catch (InterruptedException e) { }
+            }
+        } finally {
+            statusUpdateLock.unlock();
+        }
 
     }
 
+    @Override
     public void resume() {
-
+        statusUpdateLock.lock();
+        try {
+            status = DownloadStatus.IN_PROGRESS;
+            pausedCondition.signalAll();
+        } finally {
+            statusUpdateLock.unlock();
+        }
     }
 
+    @Override
     public void cancel() {
 
     }
 
-//    public DownloadStatus getStatus() {
-//        return null;
-//    }
+    void setStatus(DownloadStatus status) {
+        statusUpdateLock.lock();
+        try {
+            this.status = status;
+        } finally {
+            statusUpdateLock.unlock();
+        }
+    }
 
-    public int getDownloadSize() {
-        return 0;
+    @Override
+    public DownloadStatus getStatus() {
+        statusUpdateLock.lock();
+        try {
+            return status;
+        } finally {
+            statusUpdateLock.unlock();
+        }
+    }
+
+    @Override
+    public int getDownloadPercentage() {
+        statusUpdateLock.lock();
+        try {
+            return downloadPercentage;
+        } finally {
+            statusUpdateLock.unlock();
+        }
+    }
+
+    int increaseDownloadPercentage(int val) {
+        statusUpdateLock.lock();
+        try {
+            downloadPercentage += val;
+            return downloadPercentage;
+        } finally {
+            statusUpdateLock.unlock();
+        }
     }
 
 }
+
--- DownloadManager/src/com/aaron/Main.java
+++ DownloadManager/src/com/aaron/Main.java
@@ -1,8 +1,55 @@
 package com.aaron;
 
+import static java.lang.Thread.sleep;
+
+
+
 public class Main {
+    static class AppDownloadCompleteCallBack implements DownloadProgressCallback {
+        @Override
+        public void onComplete() {
+            System.out.println("App received notification on download complete");
+        }
+
+        @Override
+        public void onError() {
+
+        }
+    }
 
     public static void main(String[] args) {
 	// write your code here
+        DownloadManager dm = DownloadManager.getInstance();
+        if(dm == null)
+            return;
+        DownloadProgress dc1 = dm.download("URL 1", "File 1", null);
+        AppDownloadCompleteCallBack downloadcallback2 = new AppDownloadCompleteCallBack();
+        dm.download("URL 2", "File 2", downloadcallback2);
+        dm.download("URL 3", "File 3", null);
+        dm.download("URL 4", "File 4", null);
+        
+
+        try {
+            sleep(3000);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        System.out.println(dc1.getStatus());
+        System.out.println(dc1.getDownloadPercentage());
+        dc1.pause();
+        System.out.println(dc1.getStatus());
+
+        try {
+            sleep(3000);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        dc1.resume();
+        System.out.println(dc1.getStatus());
+
+
+
     }
 }
