0b30d62c8d69054d03d46a996c36ab5fb95cb40c
Li Bo Tao <Li.Tao@Oculus.com>
2020-01-22 21:24:59 -0800

    random test

--- CodingInterview/Test0122/src/com/company/Main.java
+++ CodingInterview/Test0122/src/com/company/Main.java
@@ -9,20 +9,41 @@ public class Main {
     public static void printMaxSequence(int[] a) {
         int[] max = new int[a.length];
         int[] history = new int[a.length];
+//        ArrayList<Integer>[] history = new ArrayList[a.length];
+        int max_max = 0;
+        int max_i = 0;
 
         for(int i = 0; i < a.length; i++) {
             max[i] = 0;
-            for(int j = 0; j < i; j++) {
-                if(a[i] >= a[j]) {
-                    max[i] = max[j];
-                    history[i] = j;
+            boolean maxFound = false;
+            for (int j = 0; j < i; j++) {
+                if (a[i] >= a[j]) {
+                    if(max[j] >= max[i]) {
+                        max[i] = max[j];
+                        history[i] = j;
+                        maxFound = true;
+                    }
                 }
             }
-            max[i] += 1;
+            if(maxFound)
+                max[i] += 1;
+            if(max[i] >= max_max) {
+                max_max = max[i];
+                max_i = i;
+            }
         }
 
+        ArrayList<Integer> result = new ArrayList<>();
+        int i = max_i;
+        while(max[i] != 0) {
+            result.add(a[i]);
+            i = history[i];
+        }
+        result.add(a[i]);
 
-
+        for(int j = 0; j < result.size(); j++) {
+            System.out.println(result.get(result.size() - j - 1));
+        }
 
     }
 
--- CodingInterview/strstr/src/com/company/Main.java
+++ CodingInterview/strstr/src/com/company/Main.java
@@ -2,30 +2,34 @@ package com.company;
 
 public class Main {
     public static int strstr(String termStr, String dataStr) {
+        int retval  = -1;
 
-        int resultPose = -1;
-        int curTermStr = 0;
-        for(int curDataStr = 0; curDataStr < dataStr.length(); curDataStr++) {
-            if(curTermStr == termStr.length()) {
-                resultPose = curDataStr - curTermStr;
-                return resultPose;
+        int termLength = termStr.length();
+        int dataLength = dataStr.length();
+
+        for (int i = 0; i < dataLength; i++) {
+            int j = 0;
+            for(j = 0; j < termLength ; j++) {
+                if (i + j >= dataLength || termStr.charAt(j) != dataStr.charAt(i + j))
+                    break;
             }
-            if(dataStr.charAt(curDataStr) != termStr.charAt(curTermStr)) {
-                curTermStr = 0;
-                continue;
-            } else {
-                curTermStr ++;
+            if (j == termLength) {
+                retval = i;
+                return retval;
             }
         }
-        return resultPose;
-
+        return retval;
     }
+
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println(strstr("efg", "abcdefghij"));
         System.out.println(strstr("eg", "abcdefghij"));
-        System.out.println(strstr("abc", "abcdefghij"));
+        System.out.println(strstr("ab", "aab"));
+        System.out.println(strstr("abcd", "aab"));
 
     }
 }
+
+
